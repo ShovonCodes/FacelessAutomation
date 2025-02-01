bd5b9c9a2ec05b216de45440a906457c4c56204d
Shovon <shovon@cuttingroom.com>
2025-02-01 23:25:02 +0600

    Uses channel_config.json to keep all channel specific data within the object

---
+++ ByteSizeCrypto.py
@@ -0,0 +1,80 @@
+import os
+import json
+import shutil
+from classes.story import StoryEngine
+from classes.video import VideoEngine
+from classes.audio import AudioEngine
+from classes.youtube import YoutubeEngine
+
+with open('channel_config.json', 'r') as config_file:
+    channel_config = json.load(config_file)
+
+# Extract values from json
+channel_refresh_token_key = channel_config['ByteSizeCrypto']['channel_refresh_token_key']
+asset_directory = channel_config['ByteSizeCrypto']['asset_directory']
+voice = channel_config['ByteSizeCrypto']['voice']
+topics = channel_config['ByteSizeCrypto']['topics']
+prompt_styles = channel_config['ByteSizeCrypto']['prompt_styles']
+guidelines = channel_config['ByteSizeCrypto']['guidelines']
+system_message = channel_config['ByteSizeCrypto']['system_message']
+video_description = channel_config['ByteSizeCrypto']['video_description']
+video_tags = channel_config['ByteSizeCrypto']['video_tags']
+
+# Common values
+temp_dir_path = 'tmp'
+input_video_file_path = f"{temp_dir_path}/input_video.mp4"
+input_audio_file_path = f"{temp_dir_path}/input_audio.mp3"
+input_srt_file_path = f"{temp_dir_path}/input_srt.srt"
+output_video_file_path = f"{temp_dir_path}/output_video.mp4"
+bg_music_path = f"{temp_dir_path}/bg_music.mp3"
+font_name = "The Bold Font"
+
+
+def clean_up():
+    shutil.rmtree(temp_dir_path)
+    print(f"The directory '{temp_dir_path}' has been removed successfully.")
+
+try:
+    # Creates a temporary directory
+    os.makedirs(temp_dir_path, exist_ok=True)
+
+    # Initiate engines
+    video_engine = VideoEngine(asset_directory, temp_dir_path)
+    story_engine = StoryEngine(
+        topics=topics,
+        prompt_styles=prompt_styles,
+        guidelines=guidelines,
+        system_message=system_message
+    )
+    
+    audio_engine = AudioEngine(temp_dir_path, voice)
+    youtube_engine = YoutubeEngine(
+        video_file_path=output_video_file_path,
+        refresh_token_key=channel_refresh_token_key,
+        tags=video_tags,
+        description=video_description
+    )
+    
+    # Joins existing video and creates an input video and puts it inside the temporary directory
+    video_engine.generate_input_video()
+
+    # Creates a story based on the provided category
+    story_result = story_engine.generate_story()
+    story = story_result['story']
+    story_title = story_result['title']
+
+    # # Converts the text story into audio and srt and stores them in the temporary directory
+    timestamp = audio_engine.text_to_speech_and_timestamp(story, input_audio_file_path)
+    audio_engine.timestamp_to_srt(timestamp, input_srt_file_path)
+    
+    # # # Combines the input video, audio and srt and generates a output video
+    video_engine.generate_output_video(input_video_file_path, input_audio_file_path, input_srt_file_path, output_video_file_path, font_name)
+    
+    # # # Uploads the output video to YouTube
+    youtube_engine.upload_video(story_title)
+    
+    clean_up()
+except Exception as e:
+    print('### ERROR OCCURRED')
+    print(e)
+    clean_up()
